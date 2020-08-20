1d5be318048d89a30196521068254bf0d519ac9c
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-20 06:18:44 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -76,7 +76,23 @@ def matrix_lookup(matrix, row, column)
   "Avocadoes",
   "Asparagus"]
 ]
-  return assembled_matrix[row][column]
+sorted_matrix = [
+[
+  "Eggplant",
+  "Grapefruit",
+  "Oranges",
+  "Pineapple",
+  "Watermelon"
+],
+  [
+  "Asparagus",
+  "Avocadoes",
+  "Grapes",
+  "Potatoes",
+  "Strawberries"
+]
+    ]
+  return matrix[row][column]
  
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
